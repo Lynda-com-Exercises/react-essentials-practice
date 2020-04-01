3534a000e2278214840314f4c1586c9ceb491d47
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-04-01 16:24:27 -0500

    setup and initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Message extends React.Component{
+  render(){
+    const {color, msg, minutes} = this.props;
+    return(
+      <div>
+          <h1>Hello Everyone</h1>
+          <p style={{color: color}}>{msg}</p>
+          <p>I'll check back in {minutes} minutes</p>
+      </div>
+    )
+  }
+}
+
+//const title = React.createElement('h1', {id:'title', className: 'header', style: style}, 'Hello World')
+
+ReactDom.render(<Message msg="how are you?" color="red" minutes={50}/>, document.getElementById('root'));
